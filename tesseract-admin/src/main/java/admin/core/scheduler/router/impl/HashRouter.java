64be3031c41d2b5dfdc078f3e966eb6703cfcea3
梁学凯 <liangxuekai@koolearn.com>
2019-07-15 16:23:29 +0800

    修复hash离散时可能出现的指针越界异常

--- tesseract-admin/src/main/java/admin/core/scheduler/router/impl/HashRouter.java
+++ tesseract-admin/src/main/java/admin/core/scheduler/router/impl/HashRouter.java
@@ -3,17 +3,19 @@ package admin.core.scheduler.router.impl;
 import admin.core.scheduler.router.IScheduleRouter;
 import admin.entity.TesseractExecutor;
 import admin.entity.TesseractExecutorDetail;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 /**
  * hash散列发送
  */
+@Slf4j
 public class HashRouter implements IScheduleRouter {
 
     @Override
     public TesseractExecutorDetail routerExecutor(List<TesseractExecutorDetail> tesseractExecutorList) {
-
-        return tesseractExecutorList.get(tesseractExecutorList.hashCode() % tesseractExecutorList.size());
+        //hashCode可能为负数，所以此处与一个最大值
+        return tesseractExecutorList.get(tesseractExecutorList.hashCode() & Integer.MAX_VALUE % tesseractExecutorList.size());
     }
 }
