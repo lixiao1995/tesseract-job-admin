5680c537304894bb94e03a9d0b4e0d0f466a17f3
ming_lee <zeroming@163.com>
2019-07-25 19:11:30 +0800

    fix(sql文件):删除无用sql字段

--- db/auth.sql
+++ db/auth.sql
@@ -2,10 +2,8 @@ DROP TABLE IF EXISTS `tesseract_btn_resource`;
 CREATE TABLE `tesseract_btn_resource`
 (
     `id`               int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '主键，自增',
+    `btn_code`         varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL COMMENT '按钮code',
     `btn_name`         varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '按钮名称',
-    `menu_id`          int(11)                                                 NULL DEFAULT NULL COMMENT '所属菜单ID',
-    `menu_name`        varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '父菜单名',
-    `menu_path`        varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '父菜单路径',
     `create_user_id`   int(11)                                                 NULL DEFAULT NULL COMMENT '创建人ID',
     `create_user_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL DEFAULT NULL COMMENT '创建人姓名',
     `update_user_id`   int(11)                                                 NULL DEFAULT NULL COMMENT '更新人ID',
@@ -24,6 +22,7 @@ DROP TABLE IF EXISTS `tesseract_menu_resource`;
 CREATE TABLE `tesseract_menu_resource`
 (
     `id`               int(11)                                                 NOT NULL AUTO_INCREMENT COMMENT '主键，自增',
+    `code`             varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL   COMMENT '菜单唯一code',
     `name`             varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '命名路由',
     `parent_id`        int(11)                                                 NOT NULL DEFAULT 0 COMMENT '父级菜单ID',
     `parent_name`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT NULL COMMENT '菜单的名字',
@@ -129,34 +128,34 @@ VALUES (1, 'admin', '超级管理员', 1, 'admin', 1, 'admin', 0, 0, NULL, NULL)
 
 # 菜单
 INSERT INTO `tesseract_menu_resource`
-VALUES (1, 'dashboard', 0, '', '', '/dashboard/index', '/dashboard/index', 'documentation', '控制板', 0, '', 1, 1, 'admin',
+VALUES (1, 'dashboard','dashboard', 0, '', '', '/dashboard/index', '/dashboard/index', 'documentation', '控制板', 0, '', 1, 1, 'admin',
         1, 'admin', 0, NULL, 1563606315098, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (2, 'executor', 0, '', '', '/executor/index', '/executor/index', 'documentation', '执行器列表', 0, '', 2, 1, 'admin',
+VALUES (2, 'executor','executor', 0, '', '', '/executor/index', '/executor/index', 'documentation', '执行器列表', 0, '', 2, 1, 'admin',
         1, 'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (3, 'log', 0, '', '', '/log/index', '/log/index', 'documentation', '日志列表', 0, '', 3, 1, 'admin', 1, 'admin', 0,
+VALUES (3, 'log','log', 0, '', '', '/log/index', '/log/index', 'documentation', '日志列表', 0, '', 3, 1, 'admin', 1, 'admin', 0,
         NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (4, 'group', 6, '权限管理', '', '/permission-new/group/index', '/permission-new/group/index', 'documentation',
+VALUES (4, 'group','group', 6, '权限管理', '', '/permission-new/group/index', '/permission-new/group/index', 'documentation',
         '用户组管理', 0, '', 6, 1, 'admin', 1, 'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (5, 'trigger', 0, '', '', '/trigger/index', '/trigger/index', 'documentation', '触发器列表', 0, '', 4, 1, 'admin', 1,
+VALUES (5, 'trigger','trigger', 0, '', '', '/trigger/index', '/trigger/index', 'documentation', '触发器列表', 0, '', 4, 1, 'admin', 1,
         'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (6, 'permission', 0, '', '', '/permission-new', '/permission-new', 'lock', '权限管理', 0, '', 5, 1, 'admin', 1,
+VALUES (6, 'permission','permission', 0, '', '', '/permission-new', '/permission-new', 'lock', '权限管理', 0, '', 5, 1, 'admin', 1,
         'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (7, 'menu', 6, '权限管理', '', '/permission-new/menuResource/index', '/permission-new/menuResource/index', 'lock',
+VALUES (7, 'menu','menu', 6, '权限管理', '', '/permission-new/menuResource/index', '/permission-new/menuResource/index', 'lock',
         '菜单管理', 0, '', 7, 1, 'admin', 1, 'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (8, 'role', 6, '权限管理', '', '/permission-new/role/index', '/permission-new/role/index', 'lock', '角色管理', 0, '', 8,
+VALUES (8, 'role','role', 6, '权限管理', '', '/permission-new/role/index', '/permission-new/role/index', 'lock', '角色管理', 0, '', 8,
         1, 'admin', 1, 'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (9, 'btn', 6, '权限管理', '', '/permission-new/btn/index', '/permission-new/btn/index', 'lock', '按钮管理', 0, '', 9, 1,
+VALUES (9, 'btn','btn', 6, '权限管理', '', '/permission-new/btn/index', '/permission-new/btn/index', 'lock', '按钮管理', 0, '', 9, 1,
         'admin', 1, 'admin', 0, NULL, NULL, 0);
 INSERT INTO `tesseract_menu_resource`
-VALUES (10, 'user', 6, '权限管理', '', '/permission-new/user/index', '/permission-new/user/index', 'documentation', '用户管理',
+VALUES (10, 'user', 'user', 6, '权限管理', '', '/permission-new/user/index', '/permission-new/user/index', 'documentation', '用户管理',
         0, '', 10, 1, 'admin', 1, 'admin', 0, NULL, NULL, 0);
 
 # 角色菜单
@@ -183,13 +182,13 @@ VALUES (10, 1, 10);
 
 # 按钮
 INSERT INTO `tesseract_btn_resource`
-VALUES (1, 'add', 2, '执行器列表', '/executor/index', 1, 'admin', 1, 'admin', NULL, 1563608954787, 1563608954787);
+VALUES (1, 'add', 'add', 1, 'admin', 1, 'admin', NULL, 1563608954787, 1563608954787);
 INSERT INTO `tesseract_btn_resource`
-VALUES (2, 'edit', 2, '执行器列表', '/executor/index', 1, 'admin', 1, 'admin', NULL, 1563609045332, 1563609045332);
+VALUES (2, 'edit','edit',1, 'admin', 1, 'admin', NULL, 1563609045332, 1563609045332);
 INSERT INTO `tesseract_btn_resource`
-VALUES (3, 'delete', 2, '执行器列表', '/executor/index', 1, 'admin', 1, 'admin', NULL, 1563609177143, 1563609177143);
+VALUES (3, 'delete','delete', 1, 'admin', 1, 'admin', NULL, 1563609177143, 1563609177143);
 INSERT INTO `tesseract_btn_resource`
-VALUES (4, 'select', 6, '触发器列表', '/trigger/index', 1, 'admin', 1, 'admin', NULL, 1563609314272, 1563609314272);
+VALUES (4, 'select','select', 1, 'admin', 1, 'admin', NULL, 1563609314272, 1563609314272);
 
 # 角色按钮
 
