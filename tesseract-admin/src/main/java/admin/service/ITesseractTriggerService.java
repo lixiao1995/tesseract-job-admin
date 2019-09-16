6746baf3c5e0b5097ab8f2d1eadaaed0ef45cb72
wangzhe01 <wangzhe01@koolearn.com>
2019-09-16 18:34:00 +0800

    update
    增加一点点注释

--- tesseract-admin/src/main/java/admin/service/ITesseractTriggerService.java
+++ tesseract-admin/src/main/java/admin/service/ITesseractTriggerService.java
@@ -9,14 +9,25 @@ import java.text.ParseException;
 import java.util.List;
 
 /**
- * <p>
- * 服务类
- * </p>
+ * <p>触发器服务类</p>
  *
  * @author nickle
  * @since 2019-07-03
  */
 public interface ITesseractTriggerService extends IService<TesseractTrigger> {
+
+    /**
+     * <p>Title: findTriggerWithLock</p>
+     * <p>获取触发器</p>
+     *
+     * @param tesseractGroup 触发器组
+     * @param batchSize 页数
+     * @param time 下次触发时间
+     * @param timeWindowSize 时间窗口
+     * @author wangzhe01@Koolearn-inc.com
+     * @date 2019/9/16 18:15
+     * @return list
+     */
     List<TesseractTrigger> findTriggerWithLock(TesseractGroup tesseractGroup, int batchSize, long time, Integer timeWindowSize);
 
     TriggerVO listByPage(Integer currentPage, Integer pageSize,
