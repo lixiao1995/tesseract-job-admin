218876cb26ee6e19b390c6b9595f999488b8ae7b
黄俊 <993831927@qq.com>
2019-09-24 11:36:28 +0800

    commit

--- tesseract-admin/src/test/java/nickle/tesseract/Main.java
+++ tesseract-admin/src/test/java/nickle/tesseract/Main.java
@@ -1,69 +1,104 @@
-package nickle.tesseract;
-
-import io.netty.bootstrap.Bootstrap;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.http.*;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-/**
- * @description:
- * @author: nickle
- * @create: 2019-09-09 14:14
- **/
-public class Main {
-    public static void main(String[] args) throws URISyntaxException, InterruptedException, UnsupportedEncodingException {
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-        try {
-            Bootstrap bootstrap = new Bootstrap();
-            bootstrap.group(workerGroup)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.SO_KEEPALIVE, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel socketChannel) throws Exception {
-                            socketChannel.pipeline().addLast(new HttpRequestEncoder());//客户端对发送的httpRequest进行编码
-                            socketChannel.pipeline().addLast(new HttpResponseDecoder());//客户端需要对服务端返回的httpresopnse解码
-                            // socketChannel.pipeline().addLast(new HttpClientCodec());//HttpClientCodec()包含了上面两种
-                            socketChannel.pipeline().addLast(new HttpClientDealing());
-                        }
-                    });
-
-            Channel channel = bootstrap.connect("127.0.0.1", 8080).sync().channel();
-
-            URI uri = new URI("http://127.0.0.1:8080");
-            DefaultFullHttpRequest httpRequest = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET
-                    , uri.toASCIIString(), Unpooled.wrappedBuffer("msg".getBytes("UTF-8")));//生成一个默认的httpRequest。
-
-            httpRequest.headers().set(HttpHeaders.Names.HOST, "127.0.0.1");
-            httpRequest.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-            httpRequest.headers().set(HttpHeaders.Names.CONTENT_LENGTH, httpRequest.content().readableBytes());//可以在httpRequest.headers中设置各种需要的信息。
-
-            channel.writeAndFlush(httpRequest).sync();//发送
-
-            channel.closeFuture().sync();
-
-        } finally {
-            workerGroup.shutdownGracefully();
-        }
-    }
-
-    private static class HttpClientDealing extends ChannelInboundHandlerAdapter {
-        @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            if (msg instanceof HttpResponse) {
-                System.out.println(msg.toString());//打印服务器返回的httpResponse
-            }
-
-            if (msg instanceof HttpContent) {
-                System.out.println(msg.toString());
-            }
-        }
-    }
-}
+//package nickle.tesseract;
+//
+//import java.util.*;
+//
+///**
+// * @description:
+// * @author: nickle
+// * @create: 2019-09-09 14:14
+// **/
+//public class Main {
+//    public static final String LEFT_BRACKET = "[";
+//    public static final String RIGHT_BRACKET = "]";
+//    public static final String COMMA = ",";
+//    public static final String BLANK = " ";
+//
+//    static class Node {
+//        public int id;
+//        public int pid;
+//        public int data;
+//        public List<Node> children;
+//
+//        @Override
+//        public String toString() {
+//            return "Node{" +
+//                    "id=" + id +
+//                    ", pid=" + pid +
+//                    ", data=" + data +
+//                    ", children=" + children +
+//                    '}';
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        Scanner scanner = new Scanner(System.in);
+//        String arrStr = scanner.nextLine();
+//        resolveArrStr(arrStr);
+//    }
+//
+//    public static int curIndex = 0;
+//    public static LinkedHashMap<Integer, Node> nodeLinkedHashMap = new LinkedHashMap<>();
+//    public static Stack<String> stack = new Stack<>();
+//
+//    public static void resolveArrStr(String arrStr) {
+//
+//        String curSymbol = null;
+//        while ((curSymbol = nextSymbol(arrStr)) != null) {
+//            dispatchSymbol(curSymbol);
+//        }
+//    }
+//
+//    public static boolean dispatchSymbol(String curSymbol) {
+//        if (curSymbol.equals(LEFT_BRACKET)) {
+//            stack.push(curSymbol);
+//            continue;
+//        }
+//        if (curSymbol.equals(RIGHT_BRACKET)) {
+//            if (stack.peek().equals(LEFT_BRACKET) && stack.size() == 1) {
+//                break;
+//            }
+//
+//            Integer data = Integer.valueOf(stack.pop());
+//            Integer pid = Integer.valueOf(stack.pop());
+//            Integer id = Integer.valueOf(stack.pop());
+//            Node node = nodeLinkedHashMap.get(id);
+//            Node parentNode = nodeLinkedHashMap.get(pid);
+//            if (node == null) {
+//                node = new Node();
+//                node.children = new ArrayList<>();
+//                nodeLinkedHashMap.put(id, node);
+//            }
+//            node.id = id;
+//            node.pid = pid;
+//            node.data = data;
+//
+//            if (parentNode == null) {
+//                parentNode = new Node();
+//                parentNode.id = pid;
+//                parentNode.children = new ArrayList<>();
+//                nodeLinkedHashMap.put(pid, parentNode);
+//            }
+//            parentNode.children.add(node);
+//            System.out.println(node);
+//
+//            stack.pop();
+//
+//            continue;
+//        }
+//        if (!curSymbol.equals(COMMA)) {
+//            stack.push(curSymbol);
+//        }
+//    }
+//
+//    public static String nextSymbol(String arrStr) {
+//        while (curIndex <= arrStr.length() - 1) {
+//            char c = arrStr.charAt(curIndex);
+//            ++curIndex;
+//            if (c != ' ') {
+//                return String.valueOf(c);
+//            }
+//        }
+//        return null;
+//    }
+//
+//}
