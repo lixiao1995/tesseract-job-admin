10a2f7e057a4fcee7d000cff73d001689a41bacb
黄俊 <993831927@qq.com>
2019-11-15 10:11:49 +0800

    fixed

--- tesseract-admin/src/main/java/admin/security/TokenAuthenticationFilter.java
+++ tesseract-admin/src/main/java/admin/security/TokenAuthenticationFilter.java
@@ -42,19 +42,14 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         String token = request.getHeader(AdminConstant.TOKEN);
         try {
             if (!StringUtils.isEmpty(token)) {
-                // final String authToken = authHeader.substring(tokenHead.length()); // The part after "Bearer "
-                // if (authToken != null && redisTemplate.hasKey(authToken)) {
-                //String username = redisTemplate.opsForValue().get(authToken);
                 UserAuthVO userAuthVO = tesseractUserService.getUserAuthInfo(token);
                 // 如果上面解析 token 成功并且拿到了 username 并且本次会话的权限还未被写入
                 if (userAuthVO != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
                     UserDetails userDetails = webUserDetailsService.loadUserByUsername(userAuthVO.getName());
                     // 设置用户权限
                     UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
                             userDetails, null, userDetails.getAuthorities());
                     authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    // UserContextHolder2.putUser(userAuthVO);
                     // 验证正常,生成authentication
                     logger.info("authenticated user " + userAuthVO.getName() + ", setting security context");
                     SecurityContextHolder.getContext().setAuthentication(authentication);
